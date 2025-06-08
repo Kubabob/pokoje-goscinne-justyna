60d6c8b7128d43e79f70ba77aebca5611398c9d9
Kubabob <j.m.m.bozek@gmail.com>
2025-06-08 15:37:03 +0200

    added links to navbar; changed fonts, Roboto serif for h1, h2 montserrat elsewhere;

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,14 +12,14 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className="flex gap-32 items-center">
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
+        return <CMSLink key={i} {...link} appearance="link" size={'clear'} className="text-lg" />
       })}
-      <Link href="/search">
+      {/* <Link href="/search">
         <span className="sr-only">Search</span>
         <SearchIcon className="w-5 text-primary" />
-      </Link>
+      </Link> */}
     </nav>
   )
 }
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,4 +2,8 @@ import PageTemplate, { generateMetadata } from './[slug]/page'
 
 export default PageTemplate
 
+// export default function Page() {
+//   return <p>Hello world!</p>
+// }
+
 export { generateMetadata }
