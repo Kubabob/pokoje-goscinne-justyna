8d678a2faff3bae75edd08c7729f1e1cc154859a
Kubabob <j.m.m.bozek@gmail.com>
2025-09-14 14:01:29 +0200

    Refactor next.config.js for improved readability and consistent indentation

--- next.config.js
+++ next.config.js
@@ -3,48 +3,49 @@ import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
+    images: {
+        remotePatterns: [
+            ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+                const url = new URL(item)
 
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
-    unoptimized: process.env.NODE_ENV === 'development',
-  },
-  reactStrictMode: true,
-  redirects,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
+                return {
+                    hostname: url.hostname,
+                    protocol: url.protocol.replace(':', ''),
+                    pathname: '/api/media/**',
+                }
+            }),
         ],
-      },
-    ]
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        tls: false,
-      }
-    }
-    return config
-  },
+        unoptimized: process.env.NODE_ENV === 'development',
+    },
+    reactStrictMode: true,
+    redirects,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                ],
+            },
+        ]
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+                tls: false,
+            }
+        }
+        return config
+    },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
